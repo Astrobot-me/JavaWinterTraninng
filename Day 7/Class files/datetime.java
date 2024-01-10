403538c5231631bfe50df58f358c6becc37e1284
astrobot-me <autobot268@gmail.com>
2024-01-10 22:46:33 +0530

    Day 7 file Re Structring Commit

--- Day 7/Class files/datetime.java
+++ Day 7/Class files/datetime.java
@@ -6,7 +6,7 @@ class datetime {
         // datetime d = new datetime();
         Calendar cal = Calendar.getInstance();
         Date d = new Date();
-        LocalTime t = new LocalTime();
+        // LocalTime t = new LocalTime();
         
         // System.out.println(cal.get(Calendar.DATE)+" "+cal.get(Calendar.MONTH)+" "+cal.get(Calendar.YEAR));
         System.out.println(LocalTime.now());
